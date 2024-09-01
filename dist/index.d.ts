345c2e07792e5ba6debe566851e05216eead15de
Gouthu <gouthu@MacBook-Pro.local>
2024-08-31 23:19:40 -0700

    Adding draft event

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -10,6 +10,7 @@ export * from "./models/event/EventVisibility";
 export * from "./models/event/InvitationCard";
 export * from "./models/event/LocationInfo";
 export * from "./models/event/Rsvp";
+export * from "./models/event/DraftEvent";
 export * from "./models/expenses/Expense";
 export * from "./models/expenses/Expenses";
 export * from "./models/expenses/Transaction";
