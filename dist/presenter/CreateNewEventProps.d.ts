f31f8d987539f8186f0e6196bdd3ffea36b61f77
Gouthu <gouthu@MacBook-Pro.local>
2024-09-03 00:15:30 -0700

    fixing the Event type in create new event

--- dist/presenter/CreateNewEventProps.d.ts
+++ dist/presenter/CreateNewEventProps.d.ts
@@ -1,6 +1,7 @@
 import { Profile } from "../models/profile/Profile";
 import { DraftEvent } from "../models/event/DraftEvent";
 import { LocationInfo } from "../models/event/LocationInfo";
+import { Event } from "../models/event/Event";
 export interface CreateNewEventProps {
     profile: Profile;
     searchLocation: (query: string) => Promise<LocationInfo[]>;
