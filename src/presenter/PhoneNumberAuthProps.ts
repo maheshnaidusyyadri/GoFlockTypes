a8fa6e9234555dfcf32a6c9c765c46115e501521
Gouthu <gouthu@MacBook-Pro.local>
2024-09-21 23:31:18 -0700

    Making deviceContext in phone number auth presenter

--- src/presenter/PhoneNumberAuthProps.ts
+++ src/presenter/PhoneNumberAuthProps.ts
@@ -3,7 +3,7 @@ import { Profile } from "../models/profile/Profile";
 
 export interface PhoneNumberAuthProps {
   profile: Profile;
-  deviceContext: DeviceContext;
+  deviceContext?: DeviceContext;
   sendOTP: (phoneNumberWithCountryCode: string) => Promise<boolean>;
   verifyOTP: (
     phoneNumberWithCountryCode: string,
