d462012141a41f9d03670fad4588c718e4d70b94
DileepkumarMypaty <dileepm@innvectra.com>
2024-10-29 19:03:14 +0530

    Adding the media screen props(img width,height)

--- dist/models/media/Media.d.ts
+++ dist/models/media/Media.d.ts
@@ -1,12 +1,14 @@
 import { UserMediaMetadata } from "./UserMediaMetadata";
 export interface Media {
-    id: string;
-    uploadedBy: string;
-    eventId: string;
-    type: string;
-    path: string;
-    downloadUrl?: string;
-    createdAt: string;
-    isDeleted: boolean;
-    metadata?: UserMediaMetadata;
+  id: string;
+  uploadedBy: string;
+  eventId: string;
+  type: string;
+  path: string;
+  downloadUrl?: string;
+  createdAt: string;
+  isDeleted: boolean;
+  metadata?: UserMediaMetadata;
+  width?: number;
+  height?: number;
 }
