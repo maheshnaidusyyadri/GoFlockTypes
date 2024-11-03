1669352f2e3a9d955878db926db1701b38d866c6
Gouthu <gouthu@MacBook-Pro.local>
2024-11-03 09:20:08 -0800

    Adding more attributes to UserMediaMetadata

--- src/models/media/UserMediaMetadata.ts
+++ src/models/media/UserMediaMetadata.ts
@@ -3,6 +3,9 @@ export interface UserMediaMetadata {
   name: string;
   size: number;
   type: string;
+  uploadedBy: string;
+  eventId: string;
+  mediaId?: string;
   width?: number;
   height?: number;
 }
