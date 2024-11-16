f78f80cd29fa42b0a77cb0bbb3e627c40d3f9eaa
Gouthu <gouthu@MacBook-Pro.local>
2024-11-16 14:52:23 -0800

    Adding eventRelation in props

--- dist/presenter/EventMediaProps.d.ts
+++ dist/presenter/EventMediaProps.d.ts
@@ -3,11 +3,13 @@ import { Media } from "../models/media/Media";
 import { UserMediaMetadata } from "../models/media/UserMediaMetadata";
 import { Profile } from "../models/profile/Profile";
 import { Event } from "../models/event/Event";
+import { EventRelation } from "../models/event/EventRelation";
 export interface EventMediaProps {
     profile: Profile;
     eventId: string;
     deviceContext: DeviceContext;
     event: Event;
+    eventRelation: EventRelation;
     media: Media[];
     addMedia: (mediaFiles: string[], metadataList: UserMediaMetadata[]) => Promise<Media[]>;
     deleteMedia: (mediaIds: string[]) => Promise<boolean>;
--- src/presenter/EventMediaProps.ts
+++ src/presenter/EventMediaProps.ts
@@ -3,12 +3,14 @@ import { Media } from "../models/media/Media";
 import { UserMediaMetadata } from "../models/media/UserMediaMetadata";
 import { Profile } from "../models/profile/Profile";
 import { Event } from "../models/event/Event";
+import { EventRelation } from "../models/event/EventRelation";
 
 export interface EventMediaProps {
   profile: Profile;
   eventId: string;
   deviceContext: DeviceContext;
   event: Event;
+  eventRelation: EventRelation;
   media: Media[];
 
   addMedia: (
