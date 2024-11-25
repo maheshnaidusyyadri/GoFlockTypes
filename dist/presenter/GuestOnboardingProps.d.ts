fe0d5f6382c32c1b239ef8e47b8eebbf7b4c9235
Gouthu <gouthu@MacBook-Pro.local>
2024-11-24 22:08:30 -0800

    updating attributes to GuestOnboardingProps

--- dist/presenter/GuestOnboardingProps.d.ts
+++ dist/presenter/GuestOnboardingProps.d.ts
@@ -1,6 +1,12 @@
+import { DeviceContext, Profile } from "@goflock/types";
 import { EventMinDetails } from "../models/event/EventMinDetails";
+import { EventRelation } from "../models/event/EventRelation";
 export interface GuestOnboardingProps {
+    profile: Profile;
+    deviceContext: DeviceContext;
+    eventRelation: EventRelation;
     eventMinDetails: EventMinDetails;
     sendOTP: (phoneNumber: string) => Promise<boolean>;
     verifyOTP: (phoneNumber: string, otp: string) => Promise<boolean>;
+    onSuccessfulAuth: () => void;
 }
